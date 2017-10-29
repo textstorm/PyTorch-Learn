e4f542b59fc10304d7d062a922a074e35ed367e5
textstorm <jiangzhewise@gmail.com>
2017-10-29 11:14:02 +0800

    autograde update

--- learnPytorch.py
+++ learnPytorch.py
@@ -42,3 +42,33 @@ if torch.cuda.is_available():
   x + y
 
 #autograd
+#Variable
+from torch.autograd import Variable
+x = Variable(torch.ones(2, 2), requires_grad=True)
+print x
+
+y = x + 2
+print y
+print y.grad_fn
+
+z = y * y * 3
+out = z.mean()
+print z, out
+
+#Gradients
+out.backward()
+print x.grad
+
+#more autograde
+x = torch.randn(3)
+x = Variable(x, requires_grad=True)
+
+y = x * 2
+while y.data.norm() < 1000:
+  y = y * 2
+
+print y
+
+gradients = torch.FloatTensor([0.1, 1.0, 0.0001])
+y.backward(gradients)
+print x.grad
