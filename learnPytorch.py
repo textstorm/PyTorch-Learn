eda38ffcf41752fb8fcaa85bfe7a8539cd01a51b
textstorm <jiangzhewise@gmail.com>
2017-10-29 19:32:52 +0800

    classfier update

--- learnPytorch.py
+++ learnPytorch.py
@@ -150,3 +150,62 @@ output = net(input)
 loss = criterion(output, target)
 loss.backward()
 optimizer.step()
+
+#training a classfier
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class Net(nn.Module):
+  def __init__(self):
+    super(Net, self).__init__()
+    self.conv1 = nn.Conv2d(3, 6, 5)
+    self.pool = nn.MaxPool2d(2, 2)
+    self.conv2 = nn.Conv2d(6, 16, 5)
+    self.fc1 = nn.Linear(16 * 5 * 5, 120)
+    self.fc2 = nn.Linear(128, 84)
+    self.fc3 = nn.Linear(84, 10)
+
+  def forward(self, x):
+    x = self.pool(F.relu(self.conv1(x)))
+    x = self.pool(F.relu(self.conv2(x)))
+    x = x.view(-1, 16 * 5 * 5)
+    x = F.relu(self.fc1(x))
+    x = F.relu(self.fc2(x))
+    x = self.fc3(x)
+    return x
+
+net = Net()
+net = net.cuda()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2):
+  running_loss = 0.0
+  for i, data in enumerate(trainloader, 0):
+    inputs, labels = data
+    inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
+
+    optimizer.zero_grad()
+
+    outputs = net(inputs)
+    loss = criterion(outputs, labels)
+    loss.backward()
+    optimizer.step()
+
+    running_loss += loss.data[0]
+    if i % 2000 == 1999:
+      print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+      running_loss = 0.0
+
+correct = 0
+total = 0
+for data in testloader:
+  images, labels = data
+  outputs = net(Variable(images))
+  _, predicted = torch.max(outputs.dta, 1)
+  total += labels.size(0)
+  correct += (predicted == labels).sum()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
